d5157307acc492e17ec9d9b741ce6f56a28d9506
Bhavish <bhavish.makkar@gmail.com>
2026-09-17 13:20:27 +0530

    User Registeration Flow

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
+from .forms import RegistrationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully. Please log in.")
+            return redirect("/login/")
+    else:
+        form = RegistrationForm()
+
+    return render(request, "accounts/register.html", {"form": form})
 
 # Create your views here.
