6b3b627f7314ad42dbb8120b1609760b548d51e1
Bhavish <bhavish.makkar@gmail.com>
2026-09-17 15:27:41 +0530

    Added the project creation flow

--- projects/tests.py
+++ projects/tests.py
@@ -24,4 +24,45 @@ class ProjectModelTests(TestCase):
 
         self.assertEqual(Project.objects.filter(owner=owner).count(), 2)
 
+
+class ProjectCreationTests(TestCase):
+    def setUp(self):
+        self.password = "StrongPassword123!"
+        self.owner = User.objects.create_user(username="aman", password=self.password)
+
+    def test_anonymous_user_is_redirected_to_login(self):
+        response = self.client.get("/projects/create/")
+
+        self.assertRedirects(response, "/login/?next=/projects/create/")
+
+    def test_authenticated_user_can_create_project_with_themselves_as_owner(self):
+        self.client.login(username="aman", password=self.password)
+
+        response = self.client.post(
+            "/projects/create/",
+            {"name": "Website Redesign", "description": "Update the company website."},
+        )
+
+        project = Project.objects.get(name="Website Redesign")
+        self.assertRedirects(response, f"/projects/{project.pk}/")
+        self.assertEqual(project.owner, self.owner)
+
+    def test_owner_is_not_taken_from_post_data(self):
+        self.client.login(username="aman", password=self.password)
+
+        self.client.post(
+            "/projects/create/",
+            {"name": "Safe Project", "description": "", "owner": "999", "owner_id": "999"},
+        )
+
+        self.assertEqual(Project.objects.get(name="Safe Project").owner, self.owner)
+
+    def test_invalid_project_does_not_get_created(self):
+        self.client.login(username="aman", password=self.password)
+
+        response = self.client.post("/projects/create/", {"name": "", "description": "Details"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(Project.objects.filter(description="Details").exists())
+
 # Create your tests here.
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import ProjectForm
+from .models import Project
+
+
+@login_required
+def project_create(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = request.user
+            project.save()
+            return redirect("project_detail", pk=project.pk)
+    else:
+        form = ProjectForm()
+
+    return render(request, "projects/project_form.html", {"form": form})
+
+
+@login_required
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, "projects/project_detail.html", {"project": project})
 
 # Create your views here.
